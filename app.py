31fcd9c84fc4a1ea29d456b2f54b462cd7ae8d6d
Rahul chidambaram <rahul.chidu19@gmail.com>
2026-05-05 22:46:53 +0530

    created app.py file

---
+++ app.py
@@ -0,0 +1,8 @@
+def sum(a, b):
+    return a + b
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+result = sum(a, b)
+
+print(f"The sum of {a} and {b} is: {result}")
